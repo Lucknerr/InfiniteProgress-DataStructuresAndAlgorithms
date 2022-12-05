c6db1c81677d695b5ecd814c43b34a44e7b11d36
Luckner <774069900@qq.com>
2022-12-05 13:36:04 +0800

    update LetfGod

--- LeftGod/coding/src/com/zl/stringswaptime/StringSwapTime.java
+++ LeftGod/coding/src/com/zl/stringswaptime/StringSwapTime.java
@@ -8,13 +8,19 @@ public class StringSwapTime {
      * @return 每种字符统一分布在两侧,至少需要交换的次数
      */
     public int swapTime(String s) {
+        // 定义一个左指针
         int l = 0;
+        // 记录交换次数
         int ans = 0;
         for (int i = 0; i < s.length(); i++) {
+            // 当i位置为G时
             if (s.charAt(i) == 'G') {
+                // i - l 位置就是G要交换的次数
+                // l指针向右滑动
                 ans += i - l++;
             }
         }
+        // 下文同上文一样,下文是获取(将所有的B放左边)的交换次数
         int ll = 0;
         int tmp = 0;
         for (int i = 0; i < s.length(); i++) {
